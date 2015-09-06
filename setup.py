4953df2f550d890570e9caa366ee014fd741726c
Filipe Funenga <fmafunenga@gmail.com>
2015-09-06 01:02:22 +0100

    updates setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 
 here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README')) as f:
+with open(os.path.join(here, 'README.md')) as f:
     long_description = f.read()
 execfile('lutline/version.py')
 
