ce22d8e6f36631814d361b03f9812e394b4f6b4b
Filipe Funenga <fmafunenga@gmail.com>
2015-09-06 19:53:57 +0100

    correctly formats the python output

--- lutline/format/__init__.py
+++ lutline/format/__init__.py
@@ -9,9 +9,13 @@ def export(usage, lut, language_ext="py"):
     nonenize = lambda o: "" if o == None else unicode(o)
     elementize = lambda e: ";".join(nonenize(lii) for lii in e)
     listify = lambda l: ":".join(elementize(li) for li in l) if l else ""
-    lut_str = "_".join(
+    lut_str = "|".join(
         ",".join([listify(emb), listify(exp), nonenize(imp)])
         for emb, exp, imp in lut)
+    n = 67
+    npartitions = int(len(lut_str) / float(n)) + 1
+    lut_str = [lut_str[i * n:(i * n) + n] for i in range(npartitions)]
+    lut_str = '"\n           "'.join(lut_str)
     path = os.path.dirname(os.path.realpath(__file__))
     path = os.path.join(path, "template." + language_ext)
     try:
