e0941648ac50d6f5c94415a930a219be80ddc939
Filipe Funenga <fmafunenga@gmail.com>
2015-09-15 22:49:47 +0100

    Updates index

--- compile.py
+++ compile.py
@@ -3,6 +3,7 @@
 
 
 import datetime
+import string
 import markdown2
 
 
@@ -36,40 +37,52 @@ TEMPLATE = """\
 
     <div class="jumbotron">
       <h1>lutline</h1>
-      <p class="lead">Parse <code>argv</code> using a fast look-up table generated from a command-line interface specification.</p>
+      <p class="lead">Parse <code>argv</code> using a fast look-up table generated from your command-line interface specification.</p>
     </div>
 
     <div style="float:right; margin-left:30px; width: 300px;">
         <div class="panel panel-default">
           <div class="panel-body">
-            <p>Contents:</p>
+            <p>Learn more:</p>
             <ul>
-               {toc}
+               $toc
             </ul>
           </div>
         </div>
     </div>
-    {body}
+    $body
     <br>
     <footer class="footer">
         <div style="float:left;"><a href="https://github.com/ffunenga/lutline/blob/master/LICENSE">Check the MIT License</a></div>
-        <div style="float:right;">{date}</div>
+        <div style="float:right;">$date</div>
     </footer>
     </div>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-35879263-5', 'auto');
+      ga('send', 'pageview');
+
+    </script>
   </body>
 </html>
 """
 
 with open("index.md") as f:
     index = f.read()
-headings = [l[2:].strip() for l in index.splitlines() if l.startswith("##")]
+with open("basics.md") as f:
+    basics = f.read()
+headings = [l[2:].strip() for l in basics.splitlines() if l.startswith("##")]
 tags = [h.lower().replace(" ", "-") for h in headings]
-__t = '<li><a href="#{tag}">{heading}</a></li>'
+__t = '<li><a href="basics#{tag}">{heading}</a></li>'
 toc = "\n".join(__t.format(tag=t, heading=h) for t, h in zip(tags, headings))
 body = markdown2.markdown(index, extras=['fenced-code-blocks', 'toc'])
-doc = TEMPLATE.format(toc=toc, body=body, date="This page was generated in " + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-with open("index_.html", "w") as f:
+doc = string.Template(TEMPLATE).safe_substitute(toc=toc, body=body, date="This page was generated in " + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+with open("index.html", "w") as f:
     f.write(doc)
