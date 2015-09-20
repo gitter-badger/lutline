2a5bf4df9e881957e6626ac26c5311d12f95df5d
Filipe Funenga <fmafunenga@gmail.com>
2015-09-20 11:30:57 +0100

    Updates compile.py script

--- compile.py
+++ compile.py
@@ -43,7 +43,7 @@ TEMPLATE_INDEX = """\
         <div class="row">
             <div class="col-md-7">
               $body
-              <a class="btn btn-default" href="get_started.html" style="margin-bottom:10px">Get Started <span class="glyphicon glyphicon-chevron-right"></span></a>
+              <a class="btn btn-default" href="get_started.html" style="margin-top:10px; margin-bottom:10px">Get Started <span class="glyphicon glyphicon-chevron-right"></span></a>
             </div>
             <div class="col-md-5">
                 <div class="panel panel-default">
@@ -97,7 +97,7 @@ TEMPLATE_DOC = """\
             <div class="header fixed-top clearfix">
                 <ul class="nav nav-pills">
                     <li role="presentation" style="margin-right:20px;"><a href="index.html"><span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span> Back</a></li>
-                    <li role="presentation"><h3 class="pull-right" role="presentation">Get Started</h3></li>
+                    <li role="presentation"><h3 class="pull-right" role="presentation">$title</h3></li>
                 </ul>
             </div>
             <div style="float:right; margin-left:30px; width: 300px;">
@@ -116,7 +116,7 @@ TEMPLATE_DOC = """\
                 <div style="float:right;">$date</div>
             </footer>
         </div>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script src="js/jquery.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
         <script>
             (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
@@ -147,8 +147,34 @@ with open("index.html", "w") as f:
     f.write(doc)
 
 
+title = "Tutorial - Getting started"
 body = markdown2.markdown(basics, extras=['fenced-code-blocks', 'toc'])
 doc = string.Template(TEMPLATE_DOC).safe_substitute(
-        body=body, toc=toc, date=generated)
+        title=title, body=body, toc=toc, date=generated)
 with open("get_started.html", "w") as f:
+    f.write(doc)
+
+
+with open("specfile.md") as f:
+    specfile = f.read()
+title = "The spec file"
+body = markdown2.markdown(specfile, extras=['fenced-code-blocks', 'toc'])
+doc = string.Template(TEMPLATE_DOC).safe_substitute(
+        title=title, body=body, toc=toc, date=generated)
+with open("specfile.html", "w") as f:
+    f.write(doc)
+
+
+with open("efficient.md") as f:
+    efficient = f.read()
+title = "Efficiency"
+headings = [l[2:].strip() for l in efficient.splitlines() if l.startswith("##")]
+tags = [h.lower().replace(" ", "-") for h in headings]
+__t = '<li><a href="efficient.html#{tag}">{heading}</a></li>'
+toc = "\n".join(__t.format(tag=t, heading=h) for t, h in zip(tags, headings))
+body = markdown2.markdown(efficient, extras=['fenced-code-blocks', 'toc', 'tables'])
+body = body.replace("<table>", '<table class="table table-striped">')
+doc = string.Template(TEMPLATE_DOC).safe_substitute(
+        title=title, body=body, toc=toc, date=generated)
+with open("efficient.html", "w") as f:
     f.write(doc)
